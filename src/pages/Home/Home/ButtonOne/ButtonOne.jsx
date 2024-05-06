1869511db8a43f4d09d7849cb65466a0d2298282
Talha <mdtalha2008@gmail.com>
2024-05-06 09:35:12 +0600

    update

--- src/pages/Home/Home/ButtonOne/ButtonOne.jsx
+++ src/pages/Home/Home/ButtonOne/ButtonOne.jsx
@@ -4,13 +4,26 @@ import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
 const ButtonOne = () => {
-    const codeString = '(num) => num + 1';
+    const codeString = '<button className="rounded-lg bg-sky-500 px-8 py-3 text-xl text-white duration-300 active:scale-95">Button</button>';
+    const codeString2 = '<button className="rounded-lg border-2 border-sky-500 px-8 py-3 text-xl text-sky-500 duration-200 hover:bg-sky-500 hover:text-white">Button</button>';
     return (
         <div className=''>
+              <div>
+              <button className="rounded-lg bg-sky-500 px-8 py-3 text-xl text-white duration-300 active:scale-95">Button</button>
               
               <SyntaxHighlighter language="javascript" style={docco}>
       {codeString}
     </SyntaxHighlighter>
+              </div>
+
+              <div>
+              <button className="rounded-lg border-2 border-sky-500 px-8 py-3 text-xl text-sky-500 duration-200 hover:bg-sky-500 hover:text-white">Button</button>
+              
+              <SyntaxHighlighter language="javascript" style={docco}>
+      {codeString2}
+    </SyntaxHighlighter>
+              </div>
+              
         </div>
     );
 };
