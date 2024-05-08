e98b2af970a3a31b1654a11a777dbb8cae6335b2
Talha <mdtalha2008@gmail.com>
2024-05-08 22:05:27 +0600

    button update

--- src/pages/Home/Home/ButtonOne/ButtonOne.jsx
+++ src/pages/Home/Home/ButtonOne/ButtonOne.jsx
@@ -1,34 +1,34 @@
-// import React from 'react';
-// import SyntaxHighlighter from 'react-syntax-highlighter';
-// import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
-// const ButtonOne = () => {
-//     const codeString = '<button className="rounded-lg bg-sky-500 px-8 py-3 text-xl text-white duration-300 active:scale-95">Button</button>';
-//     const codeString2 = '<button className="rounded-lg border-2 border-sky-500 px-8 py-3 text-xl text-sky-500 duration-200 hover:bg-sky-500 hover:text-white">Button</button>';
-//     return (
-//         <div className=''>
-//               <div>
-//               <button className="rounded-lg bg-sky-500 px-8 py-3 text-xl text-white duration-300 active:scale-95">Button</button>
+const ButtonOne = () => {
+    const codeString = '<button className="rounded-lg bg-sky-500 px-8 py-3 text-xl text-white duration-300 active:scale-95">Button</button>';
+    const codeString2 = '<button className="rounded-lg border-2 border-sky-500 px-8 py-3 text-xl text-sky-500 duration-200 hover:bg-sky-500 hover:text-white">Button</button>';
+    return (
+        <div className=''>
+              <div>
+              <button className="rounded-lg bg-sky-500 px-8 py-3 text-xl text-white duration-300 active:scale-95">Button</button>
               
-//               <SyntaxHighlighter language="javascript" style={docco}>
-//       {codeString}
-//     </SyntaxHighlighter>
-//               </div>
+              <SyntaxHighlighter language="javascript" style={docco}>
+      {codeString}
+    </SyntaxHighlighter>
+              </div>
 
-//               <div>
-//               <button className="rounded-lg border-2 border-sky-500 px-8 py-3 text-xl text-sky-500 duration-200 hover:bg-sky-500 hover:text-white">Button</button>
+              <div>
+              <button className="rounded-lg border-2 border-sky-500 px-8 py-3 text-xl text-sky-500 duration-200 hover:bg-sky-500 hover:text-white">Button</button>
               
-//               <SyntaxHighlighter language="javascript" style={docco}>
-//       {codeString2}
-//     </SyntaxHighlighter>
-//               </div>
+              <SyntaxHighlighter language="javascript" style={docco}>
+      {codeString2}
+    </SyntaxHighlighter>
+              </div>
               
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default ButtonOne;
+export default ButtonOne;
 
 
 
