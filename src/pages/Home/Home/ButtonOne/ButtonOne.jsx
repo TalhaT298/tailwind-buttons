7439fa51cf776440f4be3e25f148178c7deebe50
Talha <mdtalha2008@gmail.com>
2024-05-09 22:46:40 +0600

    button code add

--- src/pages/Home/Home/ButtonOne/ButtonOne.jsx
+++ src/pages/Home/Home/ButtonOne/ButtonOne.jsx
@@ -76,7 +76,7 @@ const ButtonOne = () => {
 
       <div className="mockup-code bg-black">
         <pre>
-          {`<button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 animate-bounce" onclick="alert('Clicked!')">Click me</button>`}
+          {`<button class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out animate-pulse hover:animate-ping" onclick="alert('Clicked!')">Click me</button>`}
           <code>npm i daisyui</code>
         </pre>
       </div>
