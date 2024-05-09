393c1909f6a8cd3b935567365ad72aa4e6eb013e
Talha <mdtalha2008@gmail.com>
2024-05-09 18:49:17 +0600

    button add

--- src/pages/Home/Home/ButtonOne/ButtonOne.jsx
+++ src/pages/Home/Home/ButtonOne/ButtonOne.jsx
@@ -2,7 +2,6 @@
 // import SyntaxHighlighter from 'react-syntax-highlighter';
 // import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-
 // const ButtonOne = () => {
 //     const codeString = '<button className="rounded-lg bg-sky-500 px-8 py-3 text-xl text-white duration-300 active:scale-95">Button</button>';
 //     const codeString2 = '<button className="rounded-lg border-2 border-sky-500 px-8 py-3 text-xl text-sky-500 duration-200 hover:bg-sky-500 hover:text-white">Button</button>';
@@ -10,7 +9,7 @@
 //         <div className=''>
 //               <div>
 //               <button className="rounded-lg bg-sky-500 px-8 py-3 text-xl text-white duration-300 active:scale-95">Button</button>
-              
+
 //               <SyntaxHighlighter language="javascript" style={docco}>
 //       {codeString}
 //     </SyntaxHighlighter>
@@ -18,15 +17,49 @@
 
 //               <div>
 //               <button className="rounded-lg border-2 border-sky-500 px-8 py-3 text-xl text-sky-500 duration-200 hover:bg-sky-500 hover:text-white">Button</button>
-              
+
 //               <SyntaxHighlighter language="javascript" style={docco}>
 //       {codeString2}
 //     </SyntaxHighlighter>
 //               </div>
-              
+
 //         </div>
 //     );
 // };
 
 // export default ButtonOne;
+import React from "react";
 
+const ButtonOne = () => {
+  return (
+    <div>
+      <div>
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:rotate-3 animate-hinge"
+        onClick={() => alert("Clicked!")}
+      >
+        Click me
+      </button>
+      <div className="mockup-code bg-black">
+        <pre>
+          {`<button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:rotate-3 animate-hinge" onClick={() => alert('Clicked!')}>Click me</button>`}
+          <code>npm i daisyui</code>
+        </pre>
+      </div>
+      </div>
+
+      <div>
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 animate-wiggle" onclick="alert('Clicked!')">Click me</button>
+
+      <div className="mockup-code bg-black">
+        <pre>
+          {`<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:rotate-3 animate-swing" onclick="alert('Clicked!')">Click me</button>`}
+          <code>npm i daisyui</code>
+        </pre>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonOne;
