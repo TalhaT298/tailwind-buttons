fee4722bcd352dccce6ecd23b2ca49c8846a3118
Talha <mdtalha2008@gmail.com>
2024-05-10 01:01:39 +0600

    social media add

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from "../../../assets/logo.png"
+import React from "react";
+import logo from "../../../assets/logo.png";
 import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { BsTwitterX } from "react-icons/bs";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
+import { FaThreads } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
 const Footer = () => {
-    return (
-      <div>
-        <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-      <aside className="items-center grid-flow-col">
-        <img className="w-20 h-14" src={logo} alt="" />
-        <p>Copyright © 2024 - Talha Tarique</p>
-      </aside> 
-      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-      <a href="">
-                  < FaFacebookF size="1.7rem" color="white" />
-                </a>
-        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-      </nav>
-    </footer>
-      </div>
-    );
+  return (
+    <div>
+      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+        <aside className="items-center grid-flow-col">
+          <img className="w-20 h-14" src={logo} alt="" />
+          <p>Copyright © 2024 - Talha Tarique</p>
+        </aside>
+        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <a href="">
+            <FaFacebookF size="1.7rem" color="white" />
+          </a>
+          <a href="">
+            <FaInstagram size="1.7rem" color="white" />
+          </a>
+          <a href="">
+            <FaLinkedinIn  size="1.7rem" color="white" />
+          </a>
+          <a href="">
+            <BsTwitterX  size="1.7rem" color="white" />
+          </a>
+          <a href="">
+            <FaWhatsapp size="1.7rem" color="white" />
+          </a>
+          <a href="">
+            <FaDiscord size="1.7rem" color="white" />
+          </a>
+          <a href="">
+            <FaThreads size="1.7rem" color="white" />
+          </a>
+          <a href="">
+            <BiLogoGmail  size="1.7rem" color="white" />
+          </a>
+          
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
