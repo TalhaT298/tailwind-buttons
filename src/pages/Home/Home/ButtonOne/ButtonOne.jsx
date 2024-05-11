78674f3b2cce2041e251c24292184db17fb016fd
Talha <mdtalha2008@gmail.com>
2024-05-11 13:21:23 +0600

    Sky-themed button with an arrow icon

--- src/pages/Home/Home/ButtonOne/ButtonOne.jsx
+++ src/pages/Home/Home/ButtonOne/ButtonOne.jsx
@@ -187,6 +187,16 @@ const ButtonOne = () => {
         </pre>
       </div>
       </div>
+      {/* 15 */}
+      <div>
+      <button className="group relative flex w-36 items-center rounded-lg border-2 border-sky-400 p-4 text-sky-300"><span>Get Start</span><span className="absolute right-3 box-content flex w-1/6 justify-center rounded-md bg-sky-400 duration-300 group-hover:w-5/6"><svg viewBox="0 0 24 24" fill="none" className="w-10" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g strokeLinecap="round" strokeLinejoin="round"></g><g><path d="M4 12H20M20 12L14 6M20 12L14 18" stroke="#fff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></g></svg></span></button>
+      <code>Sky-themed button with an arrow icon</code>
+      <div className="mockup-code bg-black">
+        <pre>
+          {`<button className="group relative flex w-36 items-center rounded-lg border-2 border-sky-400 p-4 text-sky-300"><span>Get Start</span><span className="absolute right-3 box-content flex w-1/6 justify-center rounded-md bg-sky-400 duration-300 group-hover:w-5/6"><svg viewBox="0 0 24 24" fill="none" className="w-10" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g strokeLinecap="round" strokeLinejoin="round"></g><g><path d="M4 12H20M20 12L14 6M20 12L14 18" stroke="#fff" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></g></svg></span></button>`}
+        </pre>
+      </div>
+      </div>
     </div>
   );
 };
